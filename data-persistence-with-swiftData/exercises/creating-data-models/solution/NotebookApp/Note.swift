1fe2bb3e50c08884c520d0265460303f1d3d7405
Jesus Guerra <jesus@toptal.com>
2024-05-06 19:51:36 -0700

    Add final

--- data-persistence-with-swiftData/exercises/creating-data-models/solution/NotebookApp/Note.swift
+++ data-persistence-with-swiftData/exercises/creating-data-models/solution/NotebookApp/Note.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftData
 
 @Model
-class Note: Identifiable, Hashable {
+final class Note: Identifiable, Hashable {
       var id = UUID()
       var title: String
       var body: String
